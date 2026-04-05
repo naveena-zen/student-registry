ab303843b757a59b64a4baf1f9217a86c27673cb
Naveena <naveenaa.dhanasekaran@gmail.com>
2026-04-05 19:22:38 +0530

    Delete models directory

--- models/studentModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const studentSchema = mongoose.Schema({
-  name:         { type: String, required: true },
-  roll:         { type: String, required: true, unique: true },
-  registration: { type: String, required: true, unique: true },
-  subjects:     { type: [String], required: true },
-  registered_on:{ type: Date, default: new Date() }
-});
-module.exports = mongoose.model('studentdata', studentSchema);
